2d1222c6ed32669a97bf68f6de090398d064a0d1
Ryan Kane <rmkane89@gmail.com>
2015-09-10 15:08:53 -0400

    Added grammar for parsing data

--- src/core/SectionUtils.java
+++ src/core/SectionUtils.java
@@ -9,6 +9,23 @@ public class SectionUtils {
 				section.getxOffset(), section.getyOffset());
 	}
 
+	// Grammar for parsing data using Backus-Naur Form
+	// ========================================================================
+	// <maj-div>       = '|'
+	// <min-div>       = ';'
+	// <sub-div>       = ','
+	// <divider>       = <maj-div> | <min-div> | <sub-div>
+	// <word>          = <char> { <char> }
+	// <number>        = <digit> { <number> }
+	// <name>          = <word> { <word> | <number> }
+	// <x-offset>      = <number>
+	// <y-offset>      = <number>
+	// <offset>        = <x-offset> , <sub-div> , <y-offset> 
+	// <x-coordinate>  = <number>
+	// <y-coordinate>  = <number>
+	// <point>         = <x-coordinate> , <sub-div> , <y-coordinate>
+	// <points>        = <point> { <minor-div> , <point> }
+	// <section>       = <name> , <maj-div> , <offset> , <maj-div> , <points>
 	public static Section parseSection(String data) {
 		Section section = new Section();
 		String[] shapeInfo = data.split("\\|");
